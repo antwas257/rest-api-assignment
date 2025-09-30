02bd4efce84406c68d93e2a4d443a7ab0bfb2a2a
antwas257 <antwas257@gmail.com>
2025-09-30 03:16:19 -0400

    Added GET PUSH PUT DELETE functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const port = 3000;
 
 // Middleware to parse JSON bodies
@@ -8,10 +9,85 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+let data = [];
+let response;
+let id,name,email;
+
+function getData(params,body){
+
+    return [params.id,body.email,body.name]
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/users',(req,res) => {
+
+    ({email,name} = req.body);
+
+    if (!email|| !name)
+        return res.status(400).send("Bad Request");
+
+    id =  uuidv4();
+
+    req.body["id"] = id;
+    data.push(req.body);
+
+    return res.status(201).json(req.body);
+
+});
+
+app.get('/users/:id', (req, res) => {
+
+    [id,email,name] = getData(req.params,req.body);
+
+    response = data.find(key => key.id === id);
+
+    if (!response) // can't find id
+        return res.status(404).send("Not Found");
+
+    return res.status(200).json(response);
+});
+
+
+
+app.put('/users/:id', (req, res) => {
+
+    [id,email,name] = getData(req.params,req.body);
+
+    response = data.find(key => key.id === id);
+
+    if (!response)
+        return res.status(404).send("Not Found");
+
+    if (!email || !name)
+        return res.status(400).send("Bad Request");
+
+    response.email = email;
+    response.name = name;
+
+     return res.status(200).json(response);
+
+
+});
+
+app.delete('/users/:id', (req, res) => {
+
+    [id,email,name] = getData(req.params,req.body);
+
+    response = data.find(key => key.id === id);
+
+    if (!response)
+        return res.status(404).send("Not Found");
+
+    data = data.filter(key => key.id !== id);
+
+    return res.status(204).send(); 
+
+
+});
+
 
 // Do not touch the code below this comment
 // **************************************************************
